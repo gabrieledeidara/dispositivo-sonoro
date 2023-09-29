a731b2b1a1eaa96603d2f927189d3c06fc9ed52a
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2023-09-29 10:18:12 -0300

    ok

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ function tocaSomCade() {
     document.querySelector('#som_tecla_cade')
 }
 <button onclick="tocaSomCade()" class="tecla tecla_Cade">Cade</button>
+document.querySelector('.tecla_cade');
+document.querySelector('.tecla_cade').onclick=function(){
+    document.querySelector('.tecla_cade').onclick=tocaaSomCade;
+}
