d3fa44b568d7e8a691f7de5d74a3a9666e02ef0d
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2023-10-20 10:47:02 -0300

    brsf

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-//Declaração/criaçãoda função tocaSomCade
+//Declaração/criação da função tocaSomCade
 function tocaSomCade() {
     document.querySelector('#som_tecla_cade').play();
 }
 //chamada ou invocação da função tocaSomCade ao clicar no botão
 document.querySelector('.tecla_cade').onclick = tocaSomCade;
+
+//Declaração/criação da função tocaSomIsso
+function tocaSomIsso() {
+    document.querySelector('#som_tecla_isso').play();
+}
+//chamada ou invocação da função tocaSomIsso ao clicar no botão
+document.querySelector('.tecla_isso').onclick = tocaSomIsso;
