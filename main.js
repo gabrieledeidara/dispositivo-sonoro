5a55c4e7493074b94b558839f049d795a942475a
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2023-10-20 10:43:50 -0300

    jhg

--- main.js
+++ main.js
@@ -4,13 +4,3 @@ function tocaSomCade() {
 }
 //chamada ou invocação da função tocaSomCade ao clicar no botão
 document.querySelector('.tecla_cade').onclick = tocaSomCade;
-<button onclick="tocaSomCade()" class="tecla tecla_Cade">Cade</button>
-document.querySelector('.tecla_cade');
-document.querySelector('.tecla_cade').onclick=function(){
-    document.querySelector('.tecla_cade').onclick=tocaSomCade;
-}
-const listadeTeclas= document.querySelectorAll('.teecla');
-listadeTeclas[0].onclick= tocaSomCade
-while(){
-
-}
