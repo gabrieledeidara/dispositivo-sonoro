0507af2e97ec5683f97962945a475636554e9f5e
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2023-10-27 10:15:46 -0300

    kry

--- main.js
+++ main.js
@@ -2,65 +2,11 @@
 function tocaSomCade() {
     document.querySelector('#som_tecla_cade').play();
 }
-//chamada ou invocação da função tocaSomCade ao clicar no botão
-document.querySelector('.tecla_cade').onclick = tocaSomCade;
 
-//Declaração/criação da função tocaSomIsso
-function tocaSomIsso() {
-    document.querySelector('#som_tecla_isso').play();
-}
-//chamada ou invocação da função tocaSomComo ao clicar no botão
-document.querySelector('.tecla_como').onclick = tocaSomComo;
-
-//Declaração/criação da função tocaSomComo
-function tocaSomComo() {
-    document.querySelector('#som_tecla_como').play();
-}
-
-//Declaração/criação da função tocaSomErrei
-function tocaSomErrei() {
-    document.querySelector('#som_tecla_errei').play();
-}
-//chamada ou invocação da função tocaSomErrei ao clicar no botão
-document.querySelector('.tecla_errei').onclick = tocaSomErrei;
-
-//Declaração/criação da função tocaSomAcorda
-function tocaSomAcorda() {
-    document.querySelector('#som_tecla_acorda').play();
-}
-//chamada ou invocação da função tocaSomAcorda ao clicar no botão
-document.querySelector('.tecla_acorda').onclick = tocaSomAcorda;
-
-//Declaração/criação da função tocaEle
-function tocaSomEle() {
-    document.querySelector('#som_tecla_ele').play();
-}
-//chamada ou invocação da função tocaSomEle ao clicar no botão
-document.querySelector('.tecla_ele').onclick = tocaSomEle;
-
-//Declaração/criação da função tocaKiko
-function tocaSomKiko() {
-    document.querySelector('#som_tecla_kiko').play();
-}
-//chamada ou invocação da função tocaSomKiko ao clicar no botão
-document.querySelector('.tecla_kiko').onclick = tocaSomKiko;
-
-//Declaração/criação da função tocaSomPix
-function tocaSomPix() {
-    document.querySelector('#som_tecla_pix').play();
-}
-//chamada ou invocação da função tocaSomPix ao clicar no botão
-document.querySelector('.tecla_pix').onclick = tocaSomPix;
-
-//Declaração/criação da função tocaSomLogo
-function tocaSomLogo() {
-    document.querySelector('#som_tecla_logo').play();
-}
-//chamada ou invocação da função tocaSomPix ao clicar no botão
-document.querySelector('.tecla_logo').onclick = tocaSomLogo;
-
-
-
+//criando a referências constante listaDeTeclas e buscando a classe coletiva .tecla
+const listaDeTeclas = document.querySelectorAll('.tecla');
+//chamando a consstante listaDeTeclas e o índice "0" do somCade
+listaDeTeclas[0].onclick = tocaSomCade;
 
 
 
