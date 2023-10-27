e44161a7f34fc8302d517dee861b22cab9102851
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2023-10-27 10:54:59 -0300

    fh

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 //Declaração/criação da função tocaSomCade
-function tocaSomCade() {
-    document.querySelector('#som_tecla_cade').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 //criando a referências constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
-while(contador < 9){
-    listaDeTeclas[contador].onclick = tocaSomCade;
+while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1]
+    const idAudio = "#som_" + efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom(idAudio)
+    }
     contador = contador + 1;
     console.log(contador);
 }
