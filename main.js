29ae645bc3007b109869a85244c621c91b336f2d
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2023-11-10 11:07:35 -0300

    comentarios

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 //criação da repetição "while" e trocado pelo "for" com variavel, condição e incremento
 for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const  teclas = listaDeTeclas[contador]; //guarda o cantador da lista 
-    const efeito = teclas.classList[1]; //chama o 2 item da classe individual do som
+    const efeito = teclas.classList[1]; //chama o 2º item da classe individual do som
     const idAudio = `#som_${efeito}`; //utiliza a junção template string ${}
     teclas.onclick = function(){ //função anônima auxiliar para chamar tocaSom
         tocaSom(idAudio); //chamada da função tocaSom com parâmetro idAudio
