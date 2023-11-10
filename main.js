34a01f458dad0639e1af940ad033eb75065a9c2e
gabrieledeidara <114776941+gabrieledeidara@users.noreply.github.com>
2023-11-10 10:52:39 -0300

    nh

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ function tocaSom(idElementoAudio) {
 
 //criando a referências constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
 let contador = 0;
 while(contador < listaDeTeclas.length){
-    const efeito = listaDeTeclas[contador].classList[1]
-    const idAudio = "#som_" + efeito;
-    listaDeTeclas[contador].onclick = function(){
-        tocaSom(idAudio)
+    const  teclas = listaDeTeclas[contador]
+    const efeito = teclas.classList[1];
+    const idAudio = `"#som_"${efeito}`;
+    teclas.onclick = function(){
+        tocaSom(idAudio);
     }
     contador = contador + 1;
     console.log(contador);
